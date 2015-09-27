8029915b815f24a9f85c8f689c5d6463b3b60f7b
Ilya Kondrashov <loof@tut.by>
2015-09-27 03:00:26 +0300

    context 50/50 schema

--- src/newharv/main.go
+++ src/newharv/main.go
@@ -2,145 +2,94 @@
 // scope of processes of the same type is pool
 // live circle of each task is a flow transfering from pool to pool
 //
-// there is a god that can interact with each pool directly.
-// the god can receive error/done signal from pool and ask other pool to finish 
+// there is a manger that can interact with each pool directly.
+// the manger can receive error/done signal from pool and ask other pool to finish
 //
 package main
 
 func main() {
-// pool to 
 
-// pool to obtain links from google
-shopsc := make(chan Shops, )
-ctx, gcancel = context.WithCancel(context.Background())
-go glinksBlob(ctx, keysc, shopsc, gerrc)
+	keyc := make(chan string, 1)
+	kerrc := make(chan error)
+	ctx, kcancel = context.WithCancel(context.Background())
+	go inputWorker(ctx, keyc, kerrc)
 
+	// pool obtains links from google, just one thread
+	linksc := make(chan LinksBlob, 1)
+	gerrc := make(chan error)
+	ctx, gcancel = context.WithCancel(context.Background())
+	go glinksWorker(ctx, keyc, linksc, gerrc)
 
-// the god runtime
-for {
-	select {
-	case err := <-gerrc
-		gcancel()
+	// pool creates shops in db, checks for unique
+	// unlimiited threads (infact limited by mgo pool size)
+	shopc := make(chan Shop, 1)
+	merrc := make(chan error)
+	go shopCreatorsPool(ctx, linksc, shopc, merrc)
+
+	// the god runtime
+	// when we got fatal error from particular pool we need to
+	//   cancel all pools befor,
+	//   cancel all workers of current pool,
+	//   and close current output channel, if no other pool is writing to it
+	for {
+		select {
+		case err := <-gerrc:
+			gcancel()
+
+		}
 
 	}
 
 }
 
-
-
-}
-
-func glinksBlob (ctx context.Context, in <-chan string, out chan<- shop, errc chan<- error) {
+func glinksWorker(ctx context.Context, in <-chan string, out chan<- LinksBlob, errc chan<- error) {
 	for key := range in {
 
 		// query 10 pages one by one, without concurrency in order to protect from ban
-		for i:=0; i<10; i++ {
-			links, err := getLinks(ctx, key, i)
-
-					// make local copy of chans
-			out := out
-			errc := errc 
-
-			//turn of one of channel
-				if err == nil {
-					errc = nil
-				} else {
-					out = nil
-				}
-
-				select {
-				case <-ctx.Done():
-					return
-				case errc <- err:
-				case out <- links:
-				}
-
-		}
-	}
-}
-
-
-
-
-func glinksBlob1 (ctx context.Context, in <-chan string, out chan<- shop, errc chan<- error) {
-	keyc := in
-	linkc := out
-	errorc := errc
-	linkc, errorc = nil,nil
-
-	for {
-
-		select {
-		case key, ok := <-in:
-			if !ok {
-				return
-			}
-			in = nil
-
-			gtaskc = make(chan gtask, 10)
-			for i:=0; i<10 ; i++ {
-				gtaskc <- gtask{key, i}
-			}
-			close(gtaskc)
-
-		case gt, ok := <- gtaskc:
-			if !ok {
-				in = keyc
-				break
-			}
-
-			links, err = getLinks(gt.key, gt.i)
+		for i := 0; i < 10; i++ {
+			links, err := ggl.getLinks(ctx, key, i)
 
 			if err != nil {
-				eout = errc 
-			} else {
-				out = nil
+				select {
+				case errc <- err:
+					continue
+				case <-ctx.Done():
+					return
+				}
 			}
 
-
-				
-				
-			
-
-		case <-ctx.Done()
-			return
+			select {
+			case out <- LinksBlob(links, key):
+			case <-ctx.Done():
+				return
+			}
 		}
 	}
-
-
 }
 
-
-
-
-func main() {
-
-	for {
-		select {
-		case kwd := <-keywordCh:
-			// turn off this case till current will be done
-			keywordCh = nil
-
+func shopCreatorsPool(ctx context.Context, in <-chan LinksBlob, out chan<- shop, errc chan<- error) {
+	for blob := range in {
+		for _, link = range blob.links {
 			go func() {
-				// create 10 url to query
-				gurlCh := make(chan string, 10)
-				for p := 0; p < 10; p++ {
-					gurlCh <- gurl(kwd, p)
+
+				shop := NewShop(link, blob.key)
+				ok, err := db.Save(ctx, shop)
+
+				if err != nil {
+					select {
+					case errc <- err:
+					case <-ctx.Done():
+					}
+					return
 				}
 
-				for gurl := range gurlCh {
-					links := gLinks(gurl)
-
-					for _, l := range links {
-						linkCh <- l
+				if ok {
+					select {
+					case out <- shop:
+					case <-ctx.Done():
 					}
-
-					// turn on link parser for next keyword
-					keywordCh = keywordChan
 				}
 			}()
-		case link := <-linkCh:
-
 		}
 	}
 }
